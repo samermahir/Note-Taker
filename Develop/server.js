807ee2937325649d4cd1eb2b798faf99202591d1
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2022-12-09 12:15:23 -0800

    js

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -6,6 +7,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.get('/notes', (req,res)=> {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
 
 
 app.listen(PORT, ()=> console.log(`app is running at http://localhost:${PORT}`))
